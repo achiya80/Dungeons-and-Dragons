f1a4fc0d6ace543af3f47c64b4b46a32c41d8f5f
achiya <achiyane@post.bgu.ac.il>
2021-06-15 10:01:12 +0300

    added board test

--- src/Tests/BoardTests/BoardTest.java
+++ src/Tests/BoardTests/BoardTest.java
@@ -1,29 +1,34 @@
 package Tests.BoardTests;
 
+import BusinessLayer.Board.Board;
+import BusinessLayer.Board.Position;
+import BusinessLayer.Tiles.Empty;
+import BusinessLayer.Tiles.Tile;
+import BusinessLayer.Tiles.Wall;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
 
+    Board b;
     @BeforeEach
     void setUp() {
-    }
-
-    @Test
-    void getTile() {
+        b = new Board();
+        List<Tile> tiles = Arrays.asList(new Empty(new Position(1,0)), new Wall(new Position(1,1)),
+                        new Empty(new Position(0,0)),new Wall(new Position(0,1)));
+        tiles.stream().forEach(t -> b.addTile(t));
     }
 
     @Test
     void testToString() {
+        String expected = "..\n##\n";
+        assertEquals(expected, b.toString(), "the board to string isn't compatabole to expected");
     }
 
-    @Test
-    void addTile() {
-    }
-
-    @Test
-    void removeTile() {
-    }
 }
